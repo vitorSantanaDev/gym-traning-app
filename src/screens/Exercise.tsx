f46bc01da2bcb0d6b06f1e911a138ce3b08d7dca
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-12-20 07:48:23 -0300

    style: some corrections

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,5 +1,14 @@
 import { TouchableOpacity } from "react-native";
-import { Box, HStack, Heading, Icon, Image, Text, VStack } from "native-base";
+import {
+  Box,
+  HStack,
+  Heading,
+  Icon,
+  Image,
+  Text,
+  VStack,
+  ScrollView,
+} from "native-base";
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
@@ -41,42 +50,44 @@ export function Exercise() {
           </HStack>
         </HStack>
       </VStack>
-      <VStack p={8}>
-        <Image
-          h={80}
-          mb={3}
-          w="full"
-          rounded="lg"
-          overflow="hidden"
-          resizeMode="cover"
-          alt="Nome do exercício"
-          source={{
-            uri: "https://supertreino.com/wp-content/uploads/2018/08/photo-1517963879433-6ad2b056d712.jpg",
-          }}
-        />
-        <Box bg="gray.600" pb={4} px={4} rounded="md">
-          <HStack
-            mt={5}
-            mb={6}
-            alignItems="center"
-            justifyContent="space-around"
-          >
-            <HStack alignItems="center">
-              <SeriesSVG />
-              <Text ml={2} color="gray.200">
-                3 séries
-              </Text>
+      <ScrollView>
+        <VStack p={8}>
+          <Image
+            h={80}
+            mb={3}
+            w="full"
+            rounded="lg"
+            overflow="hidden"
+            resizeMode="cover"
+            alt="Nome do exercício"
+            source={{
+              uri: "https://supertreino.com/wp-content/uploads/2018/08/photo-1517963879433-6ad2b056d712.jpg",
+            }}
+          />
+          <Box bg="gray.600" pb={4} px={4} rounded="md">
+            <HStack
+              mt={5}
+              mb={6}
+              alignItems="center"
+              justifyContent="space-around"
+            >
+              <HStack alignItems="center">
+                <SeriesSVG />
+                <Text ml={2} color="gray.200">
+                  3 séries
+                </Text>
+              </HStack>
+              <HStack alignItems="center">
+                <RepetitionsSVG />
+                <Text ml={2} color="gray.200">
+                  12 repetições
+                </Text>
+              </HStack>
             </HStack>
-            <HStack alignItems="center">
-              <RepetitionsSVG />
-              <Text ml={2} color="gray.200">
-                12 repetições
-              </Text>
-            </HStack>
-          </HStack>
-          <Button label="Macar como realizado" />
-        </Box>
-      </VStack>
+            <Button label="Macar como realizado" />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   );
 }
