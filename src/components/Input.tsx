e3a9b3f127a761f0920f50e79e54d3e7ca2ac937
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-12-20 18:04:19 -0300

    feat: making the sign up forms work and adding the appropriate validations

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,21 +1,41 @@
-import { IInputProps, Input as NativeBaseInput } from "native-base";
+import {
+  FormControl,
+  IInputProps,
+  Input as NativeBaseInput,
+} from "native-base";
 
-type InputProps = IInputProps;
+type InputProps = IInputProps & { errorMessage?: string };
 
-export function Input({ ...restInputProps }: InputProps) {
+export function Input({
+  isInvalid,
+  errorMessage,
+  ...restInputProps
+}: InputProps) {
+  const Iinvalid = !!errorMessage || isInvalid;
   return (
-    <NativeBaseInput
-      px={4}
-      h={14}
-      fontSize="md"
-      bg="gray.700"
-      borderWidth={0}
-      color="white"
-      fontFamily="body"
-      mb={4}
-      _focus={{ bg: "gray.700", borderWidth: 1, borderColor: "green.500" }}
-      placeholderTextColor="gray.300"
-      {...restInputProps}
-    />
+    <FormControl isInvalid={Iinvalid} mb={4}>
+      <NativeBaseInput
+        px={4}
+        h={14}
+        fontSize="md"
+        bg="gray.700"
+        borderWidth={0}
+        borderColor={"transparent"}
+        color="white"
+        fontFamily="body"
+        _focus={{
+          bg: "gray.700",
+          borderWidth: 1,
+          borderColor: "green.500",
+        }}
+        isInvalid={Iinvalid}
+        _invalid={{ borderWidth: 1, borderColor: "red.500" }}
+        placeholderTextColor="gray.300"
+        {...restInputProps}
+      />
+      <FormControl.ErrorMessage color="red.500">
+        {errorMessage}
+      </FormControl.ErrorMessage>
+    </FormControl>
   );
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,8 @@
 import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
@@ -7,11 +10,46 @@ import { Button } from "@components/Button";
 import Logo from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 
+type FormFieldsProps = {
+  email: string;
+  password: string;
+  name: string;
+  password_confirmation: string;
+};
+
+const signUpSchema = yup.object({
+  name: yup.string().required("Informe um nome"),
+  email: yup.string().required("Informe um e-mail.").email("E-mail inválido"),
+  password: yup
+    .string()
+    .required("Informe uma senha.")
+    .min(6, "A senha deve ter no mínimo 6 caracteres."),
+  password_confirmation: yup
+    .string()
+    .oneOf(["", yup.ref("password")], "As senhas devem ser iguais.")
+    .required("Por favor, confirme sua senha."),
+});
+
 export function SignUp() {
   const navigation = useNavigation();
 
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFieldsProps>({ resolver: yupResolver(signUpSchema) });
+
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  function handleSignUp({
+    name,
+    email,
+    password,
+    password_confirmation,
+  }: FormFieldsProps) {
+    console.log({ name, email, password, password_confirmation });
   }
 
   return (
@@ -38,14 +76,67 @@ export function SignUp() {
           <Heading mb={6} color="gray.100" fontSize="xl" fontFamily="heading">
             Crie sua conta
           </Heading>
-          <Input placeholder="Nome" autoCapitalize="none" />
-          <Input
-            placeholder="E-mail"
-            autoCapitalize="none"
-            keyboardType="email-address"
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                onChangeText={onChange}
+                placeholder="Nome"
+                autoCapitalize="none"
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
           />
-          <Input secureTextEntry placeholder="Senha" />
-          <Button label="Criar e acessar" />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                value={value}
+                placeholder="E-mail"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                value={value}
+                onChangeText={onChange}
+                secureTextEntry
+                placeholder="Senha"
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password_confirmation"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                value={value}
+                onChangeText={onChange}
+                secureTextEntry
+                placeholder="Confirme a senha"
+                returnKeyType="send"
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                errorMessage={errors.password_confirmation?.message}
+              />
+            )}
+          />
+          <Button
+            onPress={handleSubmit(handleSignUp)}
+            label="Criar e acessar"
+          />
         </Center>
 
         <Button
